8a075efff2a949eec11338c4cc99fd63d90729c3
Yoda43verMyRepos <yoda43vermyrepos@gmail.com>
2024-08-05 15:38:59 -0300

    Adds remaining annotations to DadosMembro before moving on to the persistenci phase

--- src/main/java/br/com/ibns/pgm/membro/DadosMembro.java
+++ src/main/java/br/com/ibns/pgm/membro/DadosMembro.java
@@ -1,8 +1,29 @@
 package br.com.ibns.pgm.membro;
 
+import br.com.ibns.pgm.endereco.DadosEndereco;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record DadosMembro(
+
+        @NotBlank
         String nome,
+
+        @NotBlank
         String telefone,
+
+        @NotBlank
+        @Email
         String email,
-        String aniversario) {
+
+        @Pattern(regexp = "^\\d{2}/\\d{2}$", message = "Data de aniversário inválida. Formato esperado: dd/MM")
+        String aniversario,
+
+        @NotNull
+        DadosEndereco endereco
+
+
+) {
 }
