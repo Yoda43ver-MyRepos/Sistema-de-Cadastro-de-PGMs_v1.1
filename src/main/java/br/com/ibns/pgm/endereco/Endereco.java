0a1f5db46672a1f6366482cd2e51bc121a14019c
Yoda43verMyRepos <yoda43vermyrepos@gmail.com>
2024-08-05 15:27:37 -0300

    Adds DB properties to the application.properties file. Updates the classes Endereco and Membro with some lombok annotations.

--- src/main/java/br/com/ibns/pgm/endereco/Endereco.java
+++ src/main/java/br/com/ibns/pgm/endereco/Endereco.java
@@ -1,5 +1,14 @@
 package br.com.ibns.pgm.endereco;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
 
     private String logradouro;
--- src/main/java/br/com/ibns/pgm/membro/Membro.java
+++ src/main/java/br/com/ibns/pgm/membro/Membro.java
@@ -2,16 +2,28 @@ package br.com.ibns.pgm.membro;
 
 
 import br.com.ibns.pgm.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name="Membro")
+@Table(name="membros")
 public class Membro {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String telefone;
     private String email;
     private String  aniversario;
 
+
+    @Embedded
     private Endereco endereco;
 
 }
