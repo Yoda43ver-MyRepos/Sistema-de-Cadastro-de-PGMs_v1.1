7796a4315bcca0755c510365f9ddb7ca63d39efd
Yoda43verMyRepos <yoda43vermyrepos@gmail.com>
2024-08-05 14:43:53 -0300

    Project Created

---
+++ src/main/java/br/com/ibns/pgm/PgmApplication.java
@@ -0,0 +1,13 @@
+package br.com.ibns.pgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ibns/pgm/PgmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ibns.pgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
