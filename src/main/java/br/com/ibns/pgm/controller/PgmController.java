95878e4c02c59e6bad379bbd9df2029024f4114c
Yoda43verMyRepos <yoda43vermyrepos@gmail.com>
2024-08-06 18:43:42 -0300

    Updates the method: 'listarPgms' to return a Pageable object.

--- src/main/java/br/com/ibns/pgm/controller/PgmController.java
+++ src/main/java/br/com/ibns/pgm/controller/PgmController.java
@@ -5,6 +5,9 @@ import br.com.ibns.pgm.pgm.DadosPgm;
 import br.com.ibns.pgm.pgm.Pgm;
 import br.com.ibns.pgm.pgm.PgmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,9 +26,19 @@ public class PgmController {
         repository.save(new Pgm(dados));
     }
 
+
+    /* Este metodo foi modificado.
+    Mantendo este compentário para consultas futuras.
     @GetMapping
     public List<DadosListagemPgms> listarPgms(){
         return repository.findAll().stream().map(DadosListagemPgms::new).toList();
+    }
+    */
+
+
+    @GetMapping
+    public Page<DadosListagemPgms> listarPgms(Pageable pagination){
+        return repository.findAll(pagination).map(DadosListagemPgms::new);
     }
 
 
